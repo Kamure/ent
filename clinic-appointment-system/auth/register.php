7a8fd2433ade6a34f78fa01a3321e0f858e9249d
ziyane <ziyane.tatiana@starthmore.edu>
2025-09-24 14:08:01 +0300

    Documentation of structure

--- clinic-appointment-system/auth/register.php
+++ clinic-appointment-system/auth/register.php
@@ -1,3 +1,26 @@
-<?
-// Registration form
-?>
+
+<?php
+class register {
+
+    private function submit_button($value){
+        echo "<input type = 'submit' value = '{$value}'>";
+    }
+
+    public function signup(){
+        ?>
+        <h2>registration form</h2>
+        <form action='submit_signup.php' method='post'>
+
+            <label for='username'>Username:</label>
+            <input type='text' id='username' name='username' required><br><br>
+            <label for='email'>Email:</label>
+            <input type='email' id='email' name='email' required><br><br>
+            <label for='password'>Password:</label>
+            <input type='password'id='password' name='password' required><br><br>
+
+            <?php $this->submit_button('sign Up'); ?> 
+    </form>
+    <?php
+    }
+    
+}
