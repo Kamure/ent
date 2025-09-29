caae8ae0774483c924c56ff5f6b55b38517e8184
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-09-29 07:38:43 +0300

    Login and logout for patients

--- clinic-appointment-system/auth/login.php
+++ clinic-appointment-system/auth/login.php
@@ -1,3 +1,22 @@
-<?
-// Login form
+<?php
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patient Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login_process.php" method="POST">
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
