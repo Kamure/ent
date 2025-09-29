2ef420a932c4777c1be65ce495afc89fd5f21e73
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-09-29 07:33:22 +0300

    Add patient registration form and processing logic

--- clinic-appointment-system/auth/reg_process.php
+++ clinic-appointment-system/auth/reg_process.php
@@ -1,1 +1,11 @@
+<?php
+include 'db.php';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$otp = rand(100000, 999999);
+$sql = "INSERT INTO patients (name, email, password, otp, verified) VALUES ('$name', '$email', '$password', '$otp', 0)";
+$conn->query($sql);
 
+header("Location: verify.php?email=$email");
+?> 
--- clinic-appointment-system/auth/register.php
+++ clinic-appointment-system/auth/register.php
@@ -15,7 +15,7 @@ class register {
             <input type='text' id='username' name='username' required><br><br>
             <label for='email'>Email:</label>
             <input type='email' id='email' name='email' required><br><br>
-            <label for='password'>Password:</label>
+            <label for='password'>password:</label>
             <input type='password'id='password' name='password' required><br><br>
 
             <?php $this->submit_button('sign Up'); ?> 
