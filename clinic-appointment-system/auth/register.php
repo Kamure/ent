c042aeeaba8edfcfaa63ec2ad7027a2438552b18
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-10-01 11:26:22 +0300

    updated register form

--- clinic-appointment-system/auth/register.php
+++ clinic-appointment-system/auth/register.php
@@ -1,26 +1,32 @@
-
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 class register {
 
     private function submit_button($value){
-        echo "<input type = 'submit' value = '{$value}'>";
+        echo "<input type='submit' value='{$value}'>";
     }
 
     public function signup(){
         ?>
-        <h2>registration form</h2>
-        <form action='submit_signup.php' method='post'>
-
+        <h2>Registration Form</h2>
+        <form action='reg_process.php' method='post'>
             <label for='username'>Username:</label>
             <input type='text' id='username' name='username' required><br><br>
+
             <label for='email'>Email:</label>
             <input type='email' id='email' name='email' required><br><br>
-            <label for='password'>password:</label>
-            <input type='password'id='password' name='password' required><br><br>
 
-            <?php $this->submit_button('sign Up'); ?> 
-    </form>
-    <?php
+            <label for='password'>Password:</label>
+            <input type='password' id='password' name='password' required><br><br>
+
+            <?php $this->submit_button('Sign Up'); ?>
+        </form>
+        <?php
     }
-    
 }
+
+$reg = new register();
+$reg->signup();
